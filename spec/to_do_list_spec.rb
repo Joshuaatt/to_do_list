c2be1d52dd2c96b817965df577646abcf9e8b1fc
Joshua Atteberry & Tucker Mayo <student@epicodus.com>
2015-01-13 10:16:24 -0800

    first commit for to-do list app

---
+++ spec/to_do_list_spec.rb
@@ -0,0 +1,33 @@
+require('rspec')
+require('to_do_list')
+
+describe(ToDoList) do
+  before() do
+    ToDoList.clear()
+  end
+  describe("#description") do
+    it("lets you give it a description") do
+      test_to_do_list = ToDoList.new("scrub the zebra")
+      expect(test_to_do_list.description()).to(eq("scrub the zebra"))
+    end
+  end
+  describe(".all") do
+    it("is empty at first") do
+      expect(ToDoList.all()).to(eq([]))
+    end
+  end
+  describe("#save") do
+    it("adds a task to the array of saved tasks") do
+      test_to_do_list = ToDoList.new("wash the lion")
+      test_to_do_list.save()
+      expect(ToDoList.all()).to(eq([test_to_do_list]))
+    end
+  end
+  describe(".clear") do
+    it("empties out all of the saved tasks") do
+      ToDoList.new("wash the lion").save()
+      ToDoList.clear()
+      expect(ToDoList.all()).to(eq([]))
+    end
+  end
+end
